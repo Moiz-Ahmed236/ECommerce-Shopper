6df966f467bae0e38715262f07c198b454d9fa1b
Moiz Ahmed <155655860+Moiz-Ahmed236@users.noreply.github.com>
2024-10-23 00:51:11 +0500

    Add files via upload

---
+++ src/Routes.jsx
@@ -0,0 +1,40 @@
+import Index from "./Screens/Index";
+import About from "./Screens/About";
+import Products from "./Screens/Products";
+import Contact from "./Screens/Contact";
+import Users from "./Screens/Users";
+import Posts from "./Screens/Posts";
+
+
+export const routes = [
+    {
+        path: "/",
+        screen: <Index/>,
+        label: "Home",
+    },
+    {
+        path: "/about",
+        screen: <About/>,
+        label: "About",
+    },
+    {
+        path: "/products",
+        screen: <Products/>,
+        label: "Products",
+    },
+    {
+        path: "/contact",
+        screen: <Contact/>,
+        label: "Contact",
+    },
+    {
+        path: "/user",
+        screen: <Users/>,
+        label: "Users",
+    },
+    {
+        path: "/posts",
+        screen: <Posts/>,
+        label: "Posts",
+    },
+]
